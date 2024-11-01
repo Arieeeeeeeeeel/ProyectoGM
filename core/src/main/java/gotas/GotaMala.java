dd50c201515af1b9d07937afa7c35f3c0b9a4dd0
Ariel Villar <ariel.villar.s@mail.pucv.cl>
2024-10-31 22:32:48 -0300

    feat: implement Recolectable in GotaMala to decrease lives on collision

--- core/src/main/java/gotas/GotaMala.java
+++ core/src/main/java/gotas/GotaMala.java
@@ -1,10 +1,10 @@
 package gotas;
 
 import com.badlogic.gdx.graphics.Texture;
+import io.github.some_example_name.Recolectable;
 import io.github.some_example_name.Tarro;
 
-public class GotaMala extends Gota {
-
+public class GotaMala extends Gota implements Recolectable {
     public GotaMala(Texture textura, float x, float y, float velocidad) {
         super(textura, x, y, velocidad);
     }
